e0bf1aa8cf032afbb07f6ef008a778ce3edd7394
emreorm <emreorman29@icloud.com>
2022-10-14 15:41:30 +0300

    JavaCamp

--- AbstractClasses/src/Main.java
+++ AbstractClasses/src/Main.java
@@ -2,8 +2,12 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GameCalculator[] gameCalculators =  new GameCalculator[] {new WomanGameCalculator(), new OlderGameCalculator(), new KidsGameCalculator(), new ManGameCalculator()};
+		for (GameCalculator gameCalculator : gameCalculators) {
+			gameCalculator.hesapla();
+		}
+		
+		
 	}
 
 }
---
+++ AbstractClasses/src/OlderGameCalculator.java
@@ -0,0 +1,10 @@
+
+public class OlderGameCalculator extends GameCalculator {
+
+	@Override
+	public void hesapla() {
+		System.out.println("puanınız : 85");
+		
+	}
+
+}
